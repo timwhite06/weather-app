0f1cd077d07f89daed534e843afaa3e2711f2310
Tim White <timwhite3003@gmail.com>
2024-10-30 09:23:11 +0000

    remove console log

--- apps/weather-app/src/app/api/forecast/route.ts
+++ apps/weather-app/src/app/api/forecast/route.ts
@@ -36,7 +36,6 @@ export async function GET(req: Request) {
         dayLabel: day.datetime === tomorrowStr ? "Tomorrow" : new Date(day.datetime).toLocaleDateString('en-US', { weekday: 'long' })
       }));
 
-      console.log(dailyForecast)
     return NextResponse.json({ dailyForecast }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
